6226237426f09c11f71a208552a5ab646c4ccbb5
DT0712 <ducduy102938@gmail.com>
2025-10-16 21:41:40 +0700

    u

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'features/auth/presentation/blocs/auth_bloc.dart';
 import 'features/auth/presentation/pages/login_page.dart';
-import 'features/auth/presentation/pages/register_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,22 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => AuthBloc(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Mock Auth App',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        initialRoute: '/login',
-        routes: {
-          '/login': (_) => LoginPage(),
-          '/register': (_) => RegisterPage(),
-          '/home': (_) => Scaffold(
-            appBar: AppBar(title: Text('Trang chủ')),
-            body: Center(child: Text('🎉 Đăng nhập thành công!')),
-          ),
-        },
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
