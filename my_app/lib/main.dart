0e4bb84851290dbbb755026218bcb51a2ecdc1ed
SmilingIndy <trananhhuy181005@gmail.com>
2025-10-19 23:05:43 +0700

    Update main.dart

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'features/auth/presentation/pages/login_page.dart';
 import 'features/auth/presentation/blocs/auth_bloc.dart';
-import 'features/auth/data/sources/auth_remote_data_source.dart';
+import 'features/auth/data/datasources/auth_remote_datasource.dart';
 import 'features/auth/domain/repositories/auth_repository.dart';
-import 'features/auth/domain/use_cases/register_use_case.dart';
-import 'features/auth/domain/use_cases/login_use_case.dart';
+import 'features/auth/domain/usecases/register_usecase.dart';
+import 'features/auth/domain/usecases/login_usecase.dart';
 
 void main() {
   // Dependencies
